b0468cfac95a23abc5e8a3ad6202cd832c4a2c64
peter <dklirser@gmail.com>
2021-08-30 11:18:07 +0900

    commit all files in project

---
+++ MDK-ARM/F051C6Tx_project/startup_stm32f051x8.d
@@ -0,0 +1,1 @@
+f051c6tx_project\startup_stm32f051x8.o: startup_stm32f051x8.s
---
+++ MDK-ARM/RTE/_F051C6Tx_project/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'F051C6Tx_project' 
+ * Target:  'F051C6Tx_project' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
