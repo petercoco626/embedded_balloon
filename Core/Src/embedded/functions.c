221a029501358c2281591c0113ec320ee6839531
planningo-peter <peter@planningo.io>
2023-09-14 12:48:30 +0900

    [feat] Led 순서 뒤집음 테스트

--- Core/Src/embedded/functions.c
+++ Core/Src/embedded/functions.c
@@ -451,24 +451,24 @@ void checkBatteryFor26pin(ballonConfigurations* configurations){
 void checkBatteryLed(ballonConfigurations* configurations){
 		
   if(configurations->currentBattery > BATTERY_CHECK_STATE_1_ADC_MIN && configurations->currentBattery <= BATTERY_CHECK_STATE_1_ADC_MAX){
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_12,GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11,GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9,GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12,GPIO_PIN_SET);
   }
   else if(configurations->currentBattery > BATTERY_CHECK_STATE_2_ADC_MIN && configurations->currentBattery <= BATTERY_CHECK_STATE_2_ADC_MAX){
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_11 | GPIO_PIN_12,GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9 | GPIO_PIN_10,GPIO_PIN_SET);										
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9 | GPIO_PIN_10,GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_11 | GPIO_PIN_12,GPIO_PIN_SET);										
   }
   else if(configurations->currentBattery > BATTERY_CHECK_STATE_3_ADC_MIN && configurations->currentBattery <= BATTERY_CHECK_STATE_3_ADC_MAX){
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12,GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9,GPIO_PIN_SET);										
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11,GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_12,GPIO_PIN_SET);										
   }
   else if(configurations->currentBattery > BATTERY_CHECK_STATE_4_ADC_MIN && configurations->currentBattery <= BATTERY_CHECK_STATE_4_ADC_MAX){
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11,GPIO_PIN_RESET);					
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_12 ,GPIO_PIN_SET);										
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12,GPIO_PIN_RESET);					
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9 ,GPIO_PIN_SET);										
   }
   else{
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_12,GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11,GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_9,GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12,GPIO_PIN_SET);
   }
 
 }
